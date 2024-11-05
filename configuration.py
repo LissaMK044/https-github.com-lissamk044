0e0aabdb59cd6dbea7fa567a97c3b4d7c72f28e6
Lissa <lizy_cardenas@hotmail.com>
2024-11-04 21:08:43 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-ed3a850f-7c02-46f2-b2b2-a69ba6cab087.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH ="/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
